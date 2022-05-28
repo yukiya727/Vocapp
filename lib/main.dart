7413b4cbd77acd70f9fa42a068d1274a2d999ad9
PilotNorman <yipeng6@ualberta.ca>
2022-05-28 11:55:56 +0800

    git push test with personal access token

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Shared House Manager',
       theme: ThemeData(
         // This is the theme of your application.
         //
