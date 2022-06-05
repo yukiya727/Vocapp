59e011e2ed0ace718c3585881310c9c3bcaacc8f
yukiya727 <yukiya727p@gmail.com>
2022-06-05 13:22:05 +0900

    extra onboarding references

---
+++ lib/pages/authentication/sign_up_page.dart
@@ -0,0 +1,179 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import '../../styles.dart';
+import '../../main.dart';
+import '../../size_configs.dart';
+import '../../validators.dart';
+import '../../widgets/widgets.dart';
+import 'login_page.dart';
+
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({Key? key}) : super(key: key);
+
+  @override
+  _SignUpPageState createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  final _signUpKey = GlobalKey<FormState>();
+
+  void onSubmit() {
+    _signUpKey.currentState!.validate();
+  }
+
+  List<FocusNode> _signUpFocusNodes = [
+    FocusNode(),
+    FocusNode(),
+    FocusNode(),
+  ];
+
+  Future setSeenonboard() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    seenOnboard = await prefs.setBool('seenOnboard', true);
+    // this will set seenOnboard to true when running onboard page for first time.
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setSeenonboard();
+    _signUpFocusNodes.forEach((element) {
+      element.addListener(() {
+        setState(() {});
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    double height = SizeConfig.blockSizeV!;
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              children: [
+                Container(
+                  child:
+                      Image.asset('assets/images/auth/signup_illustration.png'),
+                ),
+                SizedBox(
+                  height: height * 2,
+                ),
+                Text(
+                  'Create Your Account',
+                  style: kTitle2,
+                ),
+                SizedBox(
+                  height: height * 2,
+                ),
+                Form(
+                  key: _signUpKey,
+                  child: Column(
+                    children: [
+                      MyTextFormField(
+                        fillColor: Colors.white,
+                        hint: 'Name',
+                        icon: Icons.person,
+                        inputAction: TextInputAction.next,
+                        inputType: TextInputType.name,
+                        focusNode: _signUpFocusNodes[0],
+                        validator: nameValidator,
+                      ),
+                      MyTextFormField(
+                          hint: 'Email',
+                          icon: Icons.email_outlined,
+                          fillColor: Colors.white,
+                          inputType: TextInputType.emailAddress,
+                          inputAction: TextInputAction.next,
+                          focusNode: _signUpFocusNodes[1],
+                          validator: emailValidator),
+                      MyPasswordField(
+                        fillColor: Colors.white,
+                        focusNode: _signUpFocusNodes[2],
+                        validator: passwordValidator,
+                      ),
+                      MyCheckBox(
+                        text: 'Keep me signed in',
+                      ),
+                      MyCheckBox(
+                        text: 'Email me about special pricing and more',
+                      ),
+                      MainTextButton(
+                        buttonName: 'Create Account',
+                        onPressed: onSubmit,
+                        bgColor: kPrimaryColor,
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Divider(
+                          height: 3,
+                          color: kSecondaryColor.withOpacity(0.4),
+                        ),
+                      ),
+                      Text(
+                        'Or sign in with',
+                        style: kBodyText3,
+                      ),
+                      Expanded(
+                        child: Divider(
+                          height: 3,
+                          color: kSecondaryColor.withOpacity(0.4),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: LargeIconButton(
+                        buttonName: 'Google',
+                        iconImage: 'assets/images/auth/google_icon.png',
+                      ),
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Expanded(
+                        child: LargeIconButton(
+                      buttonName: 'Facebook',
+                      iconImage: 'assets/images/auth/facebook_icon.png',
+                    )),
+                  ],
+                ),
+                SizedBox(
+                  height: height * 3,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Already have an account? ',
+                      style: kBodyText3,
+                    ),
+                    SmallTextButton(
+                      buttonText: 'Sign in',
+                      page: LoginPage(),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: height * 2,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/widgets.dart
@@ -0,0 +1,7 @@
+export 'buttons/main_text_button.dart';
+export './buttons/large_icon_button.dart';
+export '../pages/onboarding/components/onboard_nav_btn.dart';
+export './fields/my_password_field.dart';
+export './fields/my_text_form_field.dart';
+export './fields/my_check_box.dart';
+export './buttons/small_text_button.dart';
